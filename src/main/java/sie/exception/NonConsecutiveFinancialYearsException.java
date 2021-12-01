e037cc97e6f3d105bd2557823a33b12ab4dd11cc
hakan <hakan@NL579>
2021-12-01 11:43:26 +0100

    BM-1246 Gjort strängkonstant i exception publik

--- src/main/java/sie/exception/NonConsecutiveFinancialYearsException.java
+++ src/main/java/sie/exception/NonConsecutiveFinancialYearsException.java
@@ -9,8 +9,11 @@ import sie.domain.FinancialYear;
  */
 public class NonConsecutiveFinancialYearsException extends SieException {
 
+    public static final String SUFFIX = " är inte direkt före nästa års startdatum";
+    public static final String PREFIX = "Slutdatum för år ";
+
     public NonConsecutiveFinancialYearsException(FinancialYear year) {
-        super("Slutdatum för år " + year.getIndex() + " är inte direkt före nästa års startdatum", Entity.FINANCIAL_YEAR);
+        super(PREFIX + year.getIndex() + SUFFIX, Entity.FINANCIAL_YEAR);
     }
 
 }
